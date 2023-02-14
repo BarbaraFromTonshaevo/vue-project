85a50b907939ecb405f49efa093ac19e177c5dbd
unknown <elkinavarvara1999@gmail.com>
2023-02-14 14:43:30 +0300

    ProductList ProductItem

---
+++ src/components/ProductList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem
+      :product="product"
+      v-for="(product, index) in products"
+      v-bind:key="index"
+    ></ProductItem>
+  </ul>
+</template>
+
+<script>
+import ProductItem from "./ProductItem.vue";
+export default {
+  props: ["products"],
+  components: { ProductItem },
+};
+</script>
